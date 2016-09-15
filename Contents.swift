bf4d384d10b41cc9a94bdfb711c30c704dbc74dc
GallupinGallup <gallupingallup@gmail.com>
2016-09-15 13:48:49 -0600

    CREATED IT

---
+++ Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+print(str)
+var 😈 = 666
+if (var = 666)
+{
+    😈 = 666/666/666/666/666/666
+}else
+{
+    😈 = 666
+}
+p
